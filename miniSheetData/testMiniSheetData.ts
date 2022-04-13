2b43e193d7a10bb4792086782988f616e0ba702d
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-04-12 19:27:20 -0500

    This actually has a shot at working

---
+++ miniSheetData/testMiniSheetData.ts
@@ -0,0 +1,11 @@
+testTheThings(){
+    let allSheetData = constructSheetData()
+
+    let localData = allSheetData.localData
+    let remoteData = allSheetData.data
+
+    let remoteDataData = remoteData.getData()
+    let remoteDataHeaders = remoteData.getHeaders()
+    localData.setHeaders(remoteDataHeaders)
+    localData.setData(remoteData);
+}
