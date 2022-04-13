37771013ace7b3fa704d1749802c68a1a6fffe2b
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-04-13 10:08:19 -0500

    trying getting only one week's data

--- miniSheetData/testMiniSheetData.ts
+++ miniSheetData/testMiniSheetData.ts
@@ -6,6 +6,19 @@ function testTheThings(){
 
     let remoteDataData = remoteData.getData()
     let remoteDataHeaders = remoteData.getHeaders()
-    localData.setHeaders(remoteDataHeaders)
-    localData.setData(remoteData);
+    // localData.setHeaders(remoteDataHeaders)  Unfortunately, this doesn't work (yet)
+    let thisWeeksData = getThisWeeksData_(remoteDataData)
+    localData.setData(thisWeeksData);
+}
+
+function getThisWeeksData_(kiData) {
+    let minDate = new Date();
+    minDate.setDate(minDate.getDate() - minDate.getDay());
+    let minMillis = minDate.getMilliseconds();
+    let output = [];
+    for (let entry of kiData) {
+        let kiMilliseconds = entry.kiDate.getMilliseconds();
+        if (kiMilliseconds >= minMillis) output.push(entry);
+    }
+    return output;
 }
