da2e3b74c281354df5366a40e4d156356e278952
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-08-26 13:21:39 -0500

    First Test of new config

--- config.ts
+++ config.ts
@@ -16,7 +16,7 @@ interface kiDataConfig {
 var sheetCoreConfig: sheetCoreConfigInfo = {
     cacheEnabled: true,
     cacheExpiration: 1800,
-    cacheKey:"HEY THERE IT ME"
+    cacheKey:"HEY THERE IT ME 2"
 }
 
 const OVERRIDE_SECRET_DATA = {
