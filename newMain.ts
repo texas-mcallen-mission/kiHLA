fe58df9632aa88b7393def189e3d7540a978e761
ElderLoForte <122490749+ElderLoForte@users.noreply.github.com>
2023-03-03 15:42:27 -0600

    Clean-upkiHLA (#20)
    * cleaned up a lot of the lets and some other junk
    
    * update eslint
    
    * big clean-up
    
    * more clean up checking again
    
    * last change

--- newMain.ts
+++ newMain.ts
@@ -1,11 +1,11 @@
 function constructSheetDataV2_(target: manySheetDataEntries): manySheetDatas {
-    let allSheetData: manySheetDatas = {};
-    let keys: string[] = ["Constructed SheetData objects for:"];
-    for (let key in target) {
-        let entry: sheetDataEntry = target[key];
+    const allSheetData: manySheetDatas = {};
+    const keys: string[] = ["Constructed SheetData objects for:"];
+    for (const key in target) { //cannot redefine entry!! if you need to change to let
+        const entry: sheetDataEntry = target[key];
 
-        let rawSheetData = new RawSheetData(entry);
-        let sheetData = new SheetData(rawSheetData);
+        const rawSheetData = new RawSheetData(entry);
+        const sheetData = new SheetData(rawSheetData);
         keys.push(key);
         allSheetData[key] = sheetData;
     }
@@ -14,30 +14,30 @@ function constructSheetDataV2_(target: manySheetDataEntries): manySheetDatas {
 }
 
 function loadExternalDataForTesting(){
-    loadConfigs()
-    let allSheetData:manySheetDatas = constructSheetDataV2_(sheetDataConfig)
+    loadConfigs();
+    const allSheetData:manySheetDatas = constructSheetDataV2_(sheetDataConfig);
     // Here's a syntax error to test with:
     // test round 2
-    let words = "finally done testing!"
+    //const words = "finally done testing!"
 
 }
 
 function splitByMissionaryTesting(){
     // Step 0: Load KI Data, set things up
     loadConfigs()
-    let allSheetData: manySheetDatas = constructSheetDataV2_(sheetDataConfig)
+    const allSheetData: manySheetDatas = constructSheetDataV2_(sheetDataConfig)
 
-    let kiSheetData = allSheetData.localData
-    let testKiData = allSheetData.perMissionary
+    const kiSheetData = allSheetData.localData
+    const testKiData = allSheetData.perMissionary
 
-    let kiData = new kiDataClass(kiSheetData.getData())
+    const kiData = new kiDataClass(kiSheetData.getData())
     
-    let kiDataObj = kiData
-    let outData:kiDataEntry[] = []
-    let targetKeys = ["name1","name2","name3"]
-    let newKeyName = "missionary"
+    const kiDataObj = kiData
+    //const outData:kiDataEntry[] = [];
+    const targetKeys = ["name1","name2","name3"]
+    const newKeyName = "missionary"
 
-    let keysToKeep = testKiData.getKeys()
+    const keysToKeep = testKiData.getKeys()
 	
     kiDataObj.breakdownAnalysis(keysToKeep,targetKeys,newKeyName,true)
 	
@@ -66,23 +66,23 @@ function testDateConverter() {
 function aggregateDebugData() {
     // Step 0: Load everything up.
     loadConfigs();
-    let allSheetData: manySheetDatas = constructSheetDataV2_(sheetDataConfig);
-    let debugFlow: SheetData = allSheetData.debugStream;
-    let debugLogData: SheetData = allSheetData.debugLT;
+    const allSheetData: manySheetDatas = constructSheetDataV2_(sheetDataConfig);
+    const debugFlow: SheetData = allSheetData.debugStream;
+    const debugLogData: SheetData = allSheetData.debugLT;
     // let outData: kiDataEntry[] = [];
     // Step 1: Get the last row of the data so that we know how far we went.  Then, load the data.
-    let lastRow = debugFlow.getValues().length; // stored so we can delete old data upon completion.  (Should require a config option to do that tho)
-    let debugData = new kiDataClass(debugFlow.getData());
+    const lastRow = debugFlow.getValues().length; // stored so we can delete old data upon completion.  (Should require a config option to do that tho)
+    const debugData = new kiDataClass(debugFlow.getData());
 
     // Step 2: Calculate hour buckets to group data with.
     debugData.addGranulatedTime("timeStarted", "hourBucket", timeGranularities.hour);
-    let inData = debugData.end;
+    // const inData : kiDataEntry[] = debugData.end;
 
     // This is essentially configuration for what things we want to aggregate with.
-    let keysToKeep = ["timeStarted", "commit_sha", "triggerType", "github_branch_ref"];
-    let keysToLumpBy = ["github_branch_ref", "commit_sha", "triggerType", "hourBucket"];
-    let keysToAggregate = ["baseFunction"];
-    let shardKey = "shardInstanceID";
+    const keysToKeep = ["timeStarted", "commit_sha", "triggerType", "github_branch_ref"];
+    const keysToLumpBy = ["github_branch_ref", "commit_sha", "triggerType", "hourBucket"];
+    const keysToAggregate = ["baseFunction"];
+    const shardKey = "shardInstanceID";
 
     // Step 3: aggregate the data, then add the new keys to the sheetData object.
     debugData.aggregateByKeys(keysToLumpBy, keysToKeep, keysToAggregate, shardKey)
@@ -102,9 +102,9 @@ function aggregateDebugData() {
 
 function testBattery() {
     loadConfigs()
-    let startTime = new Date();
+    const startTime = new Date();
     console.log("Starting All Tests");
-    let tests = {
+    const tests = {
         tmm: updateTMMReport,
         techSquad: updateTechSquadReport,
         serviceRep: updateServiceRepReport,
@@ -114,24 +114,25 @@ function testBattery() {
         updateBapPie: createBapChart,
         cleanDebugLog:aggregateDebugData,
     };
-    for (let entry in tests) {
-        let test = tests[entry];
+
+    for (const entry in tests) { //cannot redefine entry!! if you need to change to let
+        const test = tests[entry];
         console.log("beginning test for", entry);
         test();
     }
-    let endTime = new Date();
+    const endTime = new Date();
     console.log("tests finished, took ", endTime.getTime() - startTime.getTime(), " milliseconds");
 }
 
 function updateLocalDataStore() {
     loadConfigs()
-    let allSheetData = constructSheetDataV2_(sheetDataConfig);
+    const allSheetData = constructSheetDataV2_(sheetDataConfig);
     // let remoteSheetData = constructSheetDataV2(sheetData);
     // let dataSource = remoteSheetData.remoteData;
-    let data = allSheetData.remoteData.getData();
+    const data = allSheetData.remoteData.getData();
 
     allSheetData.localData.addKeys(allSheetData.remoteData);
-    let kicData = new kiDataClass(data);
+    const kicData = new kiDataClass(data);
     kicData.calculatePercentage("rca", "rc", CONFIG.kiData.new_key_names.retentionRate);
     kicData.createSumOfKeys(CONFIG.kiData.fb_referral_keys, CONFIG.kiData.new_key_names.fb_referral_sum);
 
@@ -143,7 +144,7 @@ function updateLocalDataStore() {
 
 function testSyncDataFlowCols() {
     loadConfigs()
-    let allSheetData: manySheetDatas = constructSheetDataV2_(sheetDataConfig);
+    const allSheetData: manySheetDatas = constructSheetDataV2_(sheetDataConfig);
     allSheetData.localData.addKeys(allSheetData.form);
 
 
@@ -157,16 +158,16 @@ function testSyncDataFlowCols() {
 function updateTMMReport() {
     loadConfigs()
     // let localSheetData = constructSheetDataV2(sheetDataConfig.local);
-    let allSheetData = constructSheetDataV2_(sheetDataConfig);
+    const allSheetData = constructSheetDataV2_(sheetDataConfig);
     // sheetDataConfig.remote.
-    let kicData = new kiDataClass( allSheetData.localData.getData())
+    const kicData = new kiDataClass( allSheetData.localData.getData())
 
     // let data = dataSheet.getData();
     // let kicData = new kiDataClass(data);
-    let tmmReport = allSheetData.tmmReport;
+    const tmmReport = allSheetData.tmmReport;
     kicData.calculatePercentage("rca", "rc", CONFIG.kiData.new_key_names.retentionRate);
     kicData.createSumOfKeys(CONFIG.kiData.fb_referral_keys, CONFIG.kiData.new_key_names.fb_referral_sum);
-    let tmmReportData = kicData.removeDuplicates().getThisWeeksData().addShortLang().calculateCombinedName().end;
+    const tmmReportData = kicData.removeDuplicates().getThisWeeksData().addShortLang().calculateCombinedName().end;
     // this gets rid of any and all data that might be left behind- in practice, this clears the sheet when there are no responses for the current week.
     // tmmReport.clearContent()
     tmmReport.setData(tmmReportData);
@@ -175,19 +176,19 @@ function updateTMMReport() {
 function updateTechSquadReport() {
     // loadConfig()
     loadConfigs()
-    let allSheetData = constructSheetDataV2_(sheetDataConfig);
+    const allSheetData = constructSheetDataV2_(sheetDataConfig);
     
-    let dataSheet = allSheetData.localData;
+    const dataSheet = allSheetData.localData;
 
-    let data = dataSheet.getData();
-    let kicData = new kiDataClass(data);
-    let techReport = allSheetData.techSquad;
+    const data = dataSheet.getData();
+    const kicData = new kiDataClass(data);
+    const techReport = allSheetData.techSquad;
 
 
-    let startDate = new Date("2022-01-20"); // TODO: I forgot what day we actually started calculating these
+    const startDate = new Date("2022-01-20"); // TODO: I forgot what day we actually started calculating these
     kicData.createSumOfKeys(CONFIG.kiData.fb_referral_keys, CONFIG.kiData.new_key_names.fb_referral_sum);
 
-    let refData = kicData.removeDuplicates().removeBeforeDate(startDate).calculateCombinedName().end;
+    const refData = kicData.removeDuplicates().removeBeforeDate(startDate).calculateCombinedName().end;
 
     techReport.setData(refData);
 }
@@ -195,21 +196,21 @@ function updateTechSquadReport() {
 function createFBpieChart() {
 
     loadConfigs();
-    let allSheetData = constructSheetDataV2_(sheetDataConfig);
+    const allSheetData = constructSheetDataV2_(sheetDataConfig);
 
-    let dataSheet = allSheetData.localData;
+    const dataSheet = allSheetData.localData;
 
-    let data = dataSheet.getData();
-    let kicData = new kiDataClass(data);
-    let fbBreakDown = allSheetData.facebookBreakdown;
+    const data = dataSheet.getData();
+    const kicData = new kiDataClass(data);
+    const fbBreakDown = allSheetData.facebookBreakdown;
 
 
-    let startDate = new Date("2022-01-20"); // TODO: I forgot what day we actually started calculating these
+    const startDate = new Date("2022-01-20"); // TODO: I forgot what day we actually started calculating these
     kicData.createSumOfKeys(CONFIG.kiData.fb_referral_keys, CONFIG.kiData.new_key_names.fb_referral_sum);
 
-    let keysToKeep = ["areaName","areaEmail","isDuplicate","areaID","combinedNames","facebookRefs","kiDate", "zone", "district"]
-    let breakdownKeyName = "facebookRefs"
-    let refData = kicData.removeDuplicates().calculateCombinedName().breakdownAnalysis(keysToKeep, CONFIG.kiData.fb_referral_keys, breakdownKeyName).end;
+    const keysToKeep = ["areaName","areaEmail","isDuplicate","areaID","combinedNames","facebookRefs","kiDate", "zone", "district"]
+    const breakdownKeyName = "facebookRefs"
+    const refData = kicData.removeDuplicates().calculateCombinedName().breakdownAnalysis(keysToKeep, CONFIG.kiData.fb_referral_keys, breakdownKeyName).end;
 
     fbBreakDown.setData(refData);
 }
@@ -217,21 +218,21 @@ function createFBpieChart() {
 function createBapChart() {
     
     loadConfigs();
-    let allSheetData = constructSheetDataV2_(sheetDataConfig);
+    const allSheetData = constructSheetDataV2_(sheetDataConfig);
 
-    let dataSheet = allSheetData.localData;
+    const dataSheet = allSheetData.localData;
 
-    let data = dataSheet.getData();
-    let kicData = new kiDataClass(data);
-    let fbBreakdown = allSheetData.baptismBreakdown;
+    const data = dataSheet.getData();
+    const kicData = new kiDataClass(data);
+    const fbBreakdown = allSheetData.baptismBreakdown;
 
 
-    let startDate = new Date("2022-01-20"); // TODO: I forgot what day we actually started calculating these
+    const startDate = new Date("2022-01-20"); // TODO: I forgot what day we actually started calculating these
     // kicData.createSumOfKeys(CONFIG.kiData.fb_referral_keys, CONFIG.kiData.new_key_names.fb_referral_sum);
 
-    let keysToKeep = ["areaName", "areaEmail", "isDuplicate", "areaID", "combinedNames", "kiDate", "zone", "district"];
-    let breakdownKeyName = "baptisms";
-    let refData = kicData.removeDuplicates().calculateCombinedName().breakdownAnalysis(keysToKeep, CONFIG.kiData.baptism_source_keys, breakdownKeyName).end;
+    const keysToKeep = ["areaName", "areaEmail", "isDuplicate", "areaID", "combinedNames", "kiDate", "zone", "district"];
+    const breakdownKeyName = "baptisms";
+    const refData = kicData.removeDuplicates().calculateCombinedName().breakdownAnalysis(keysToKeep, CONFIG.kiData.baptism_source_keys, breakdownKeyName).end;
 
     fbBreakdown.setData(refData);
 }
@@ -239,17 +240,17 @@ function createBapChart() {
 
 function updateServiceRepReport() {
     loadConfigs()
-    let allSheetData = constructSheetDataV2_(sheetDataConfig);
+    const allSheetData = constructSheetDataV2_(sheetDataConfig);
     // let remoteSheetData = constructSheetDataV2(sheetDataConfig.remote);
 
-    let dataSheet = allSheetData.localData;
+    const dataSheet = allSheetData.localData;
 
-    let data = dataSheet.getData();
-    let kicData = new kiDataClass(data);
-    let serviceReport = allSheetData.serviceRep;
+    const data = dataSheet.getData();
+    const kicData = new kiDataClass(data);
+    const serviceReport = allSheetData.serviceRep;
 
-    let startDate = new Date("2022-01-20");
-    let serviceData = kicData.removeDuplicates().removeBeforeDate(startDate).calculateCombinedName().end;
+    const startDate = new Date("2022-01-20");
+    const serviceData = kicData.removeDuplicates().removeBeforeDate(startDate).calculateCombinedName().end;
 
     serviceReport.setData(serviceData);
 }
